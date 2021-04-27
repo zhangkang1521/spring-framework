671c3d724abae13178c17267ae67fc40fa89e82c
zhangkang <zhangkang@lvmama.com>
2021-04-27 16:01:30 +0800

    spring集成hibernate

---
+++ spring-demo/src/main/java/org/zk/domain/UserEntity.java
@@ -0,0 +1,31 @@
+package org.zk.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "username")
+	private String username;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
---
+++ spring-demo/src/test/java/org/zk/spring/SpringHibernateTest.java
@@ -0,0 +1,16 @@
+package org.zk.spring;
+
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.zk.domain.UserEntity;
+
+public class SpringHibernateTest {
+
+	@Test
+	public void testQuery() {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-hibernate.xml");
+		SessionFactory sessionFactory = ctx.getBean(SessionFactory.class);
+		UserEntity user = (UserEntity) sessionFactory.openSession().get(UserEntity.class, 1);
+	}
+}
