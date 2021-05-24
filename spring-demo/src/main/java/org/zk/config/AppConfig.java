946bbdd780d578ca277614155aa24b7ea851b814
zhangkang <zhangkang@lvmama.com>
2021-05-24 16:05:56 +0800

    ssi全注解整合 @MapperScan进行扫描；mapper配置文件String转换为Resource[]

--- spring-demo/src/main/java/org/zk/config/AppConfig.java
+++ spring-demo/src/main/java/org/zk/config/AppConfig.java
@@ -3,6 +3,7 @@ package org.zk.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +11,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourceArrayPropertyEditor;
 import org.zk.dao.UserDao;
 
 import javax.sql.DataSource;
+import java.beans.PropertyEditor;
 
 
 @Configuration
+@MapperScan("org.zk.dao")
 @ComponentScan("org.zk.service")
 public class AppConfig {
 
@@ -33,11 +37,16 @@ public class AppConfig {
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource);
-		sqlSessionFactoryBean.setMapperLocations(new Resource[]{new ClassPathResource("mappers/UserMapper.xml")});
+		// 配置文件string会自动转换到Resource[]
+		PropertyEditor editor = new ResourceArrayPropertyEditor();
+		editor.setAsText("classpath:mappers/*.xml");
+		Resource[] resources = (Resource[]) editor.getValue();
+		sqlSessionFactoryBean.setMapperLocations(resources);
 		return sqlSessionFactoryBean.getObject();
 	}
 
-	// 由配置后处理器注册，如何触发他的容器注册
+	// 不能这么配，因为这里是由ConfigurationClassPostProcessor注册的，不可能再回调这个bean的容器注册方法
+	// 使用ImportBeanDefinitionRegistrar进行注册
 //	@Bean
 //	public MapperScannerConfigurer mapperScannerConfigurer() {
 //		MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
@@ -46,13 +55,13 @@ public class AppConfig {
 //		return mapperScannerConfigurer;
 //	}
 
-	@Bean
-	public UserDao userDao(SqlSessionFactory sqlSessionFactory) throws Exception {
-		MapperFactoryBean<UserDao> mapperFactoryBean = new MapperFactoryBean<>();
-		mapperFactoryBean.setMapperInterface(UserDao.class);
-		mapperFactoryBean.setSqlSessionFactory(sqlSessionFactory);
-		return mapperFactoryBean.getObject();
-	}
+//	@Bean
+//	public UserDao userDao(SqlSessionFactory sqlSessionFactory) throws Exception {
+//		MapperFactoryBean<UserDao> mapperFactoryBean = new MapperFactoryBean<>();
+//		mapperFactoryBean.setMapperInterface(UserDao.class);
+//		mapperFactoryBean.setSqlSessionFactory(sqlSessionFactory);
+//		return mapperFactoryBean.getObject();
+//	}
 
 
 }
