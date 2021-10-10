9a99174586dc08ed2361b11a4935f38c9e142a5c
zhangkang <zhangkang3306@zto.com>
2021-10-10 20:59:11 +0800

    TransactionTemplate使用

--- spring-demo/src/test/java/org/zk/spring/SpringTxTest.java
+++ spring-demo/src/test/java/org/zk/spring/SpringTxTest.java
@@ -3,6 +3,9 @@ package org.zk.spring;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.zk.tx.AService;
 import org.zk.tx.AServiceImpl;
 
@@ -21,6 +24,21 @@ public class SpringTxTest {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-tx.xml");
 		AServiceImpl aService = ctx.getBean(AServiceImpl.class);
 		aService.save();
+	}
+
+	@Test
+	public void testTransactionTemplate() throws Exception {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-tx.xml");
+		TransactionTemplate transactionTemplate = ctx.getBean(TransactionTemplate.class);
+		transactionTemplate.execute(new TransactionCallback<Object>() {
+			@Override
+			public Object doInTransaction(TransactionStatus status) {
+				status.setRollbackOnly();
+				return null;
+				// throw new RuntimeException("xx");
+				// return null;
+			}
+		});
 	}
 
 
